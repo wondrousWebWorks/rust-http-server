781882f0d4cf3efa68d2bfb85c63ca495c952125
Johann Alberts <alberts.johann@gmail.com>
2021-01-15 16:34:20 +0000

    Add implementation of Server struct with new()_ associated function and run() method

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
 fn main() {
-    let server = Server::new("127.0.0.1:8080");
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
 }
 
 struct Server {
     addr: String,
 }
 
+impl Server {
+    fn new(addr: String) -> Self {
+        Self {
+            addr
+        }
+    }
+
+    fn run(self) {
+        println!("Listening on {}", self.addr);
+    }
+}
