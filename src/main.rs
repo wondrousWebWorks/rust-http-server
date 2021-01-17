565a46cd0905eb830466e3e375e20a7f2821e77e
Johann Alberts <alberts.johann@gmail.com>
2021-01-17 14:52:32 +0000

    Create http server and http method modules, as well as server module

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,11 @@
+use server::Server;
+use http::Method;
+use http::Request;
+
+mod server;
+mod http;
+
 fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
-}
-
-struct Server {
-    addr: String,
-}
-
-impl Server {
-    fn new(addr: String) -> Self {
-        Self {
-            addr
-        }
-    }
-
-    fn run(self) {
-        println!("Listening on {}", self.addr);
-    }
 }
