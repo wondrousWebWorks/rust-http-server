3abc3f41f9514580284fa0ac16571b30d99e88e4
Johann Alberts <alberts.johann@gmail.com>
2021-01-24 16:58:26 +0000

    Add loop and match the listener.accept() to unwrap it and handle errors

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,5 @@
+use std::net::{TcpListener, TcpStream};
+
 pub struct Server {
   addr: String,
 }
@@ -11,5 +13,16 @@ impl Server {
 
   pub fn run(self) {
       println!("Listening on {}", self.addr);
+
+      let listener = TcpListener::bind(&self.addr).unwrap();
+
+      loop {
+        match listener.accept() {
+          Ok((stream, _)) => {
+            println!("OK");
+          },
+          Err(e) => println!("Err: {}", e),
+        }
+      }
   }
 }
